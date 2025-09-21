13abe0b0ba545f2a393b250b251a8dc4e3ff7d2b
envoker <komogorcevanton10@gmail.com>
2025-09-21 18:13:06 +0800

    feat: maked the Header

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,5 +1,19 @@
+import '../css/header.css'
+
 export default function Header() {
     return (
-        <header></header>
+        <header>
+            <a className='logo' href="#">
+                <img src="/img/header/logo.svg" alt="" />
+                <h1>Open Enterprise</h1>
+            </a>
+
+            <nav>
+                <a href="#">Why Open Enterprise</a>
+                <a href="#">Features</a>
+                <a href="#">Contribute</a>
+                <a className='request-a' href="#">Request early access</a>
+            </nav>
+        </header>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
 
+import './fonts.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
